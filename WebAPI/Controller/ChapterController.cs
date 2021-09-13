4e9b2e1ecc0c2cbac1ca22f56a68f8d53517545e
Iryna I. Potiienko <iryna.potiienko@gmail.com>
2021-09-13 17:31:08 +0300

    Added users' permitions to controllers

--- WebAPI/Controller/ChapterController.cs
+++ WebAPI/Controller/ChapterController.cs
@@ -2,6 +2,7 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Threading.Tasks;
 using Domain.Service;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Model.Dto;
 
@@ -9,6 +10,7 @@ namespace WebAPI.Controller
 {
     [Route("[controller]")]
     [ApiController]
+    [Authorize]
     public class ChapterController : ControllerBase
     {
 
@@ -18,22 +20,25 @@ namespace WebAPI.Controller
         {
             _chapterService = chapterService;
         }
-        
 
+
+        [Authorize(Roles = "admin")]
         [HttpPost("Create")]
         public async Task<ActionResult<ChapterDto>> Create(ChapterDto chapterDto)
         {
             var created = _chapterService.Create(chapterDto);
-            
-            return CreatedAtAction("GetChapter", new { id = created.Id }, created);
+
+            return CreatedAtAction("GetChapter", new {id = created.Id}, created);
         }
-        
+
+        [AllowAnonymous]
         [HttpGet("List")]
         public List<ChapterDto> GetAll()
         {
             return _chapterService.GetAll();
         }
         
+        [AllowAnonymous]
         [HttpGet("{id:int}")]
         public async Task<ActionResult<ChapterDto>> GetChapter([Required] int id)
         {
@@ -46,6 +51,7 @@ namespace WebAPI.Controller
             return chapterDto;
         }
         
+        [Authorize(Roles = "admin")]
         [HttpPut("{id}/Update")]
         public async Task<IActionResult> PutChapter(int id, [Required] ChapterDto chapterDto)
         {
@@ -58,7 +64,7 @@ namespace WebAPI.Controller
             return NoContent();
         }
 
-        
+        [Authorize(Roles = "admin")]
         [HttpDelete("{id}")]
         public async Task<IActionResult> DeleteChapter(int id)
         {
